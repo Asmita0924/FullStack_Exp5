9e8d5fc085c3a151fe4b601b8ae3be49288dae95
Asmita Singh <asmitainnovation@gmail.com>
2026-02-16 15:14:41 +0530

    Update App.jsx

--- FL_exp5.1/src/App.jsx
+++ FL_exp5.1/src/App.jsx
@@ -2,19 +2,22 @@ import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
-// Lazy Loaded Component
+// Lazy Load
 const Dashboard = lazy(() => import("./components/Dashboard"));
 
 function App() {
   return (
-    <div>
-      <Suspense fallback={<h1 style={{ textAlign: "center" }}>Loading...</h1>}>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-      </Suspense>
-    </div>
+    <Suspense
+      fallback={
+        <h1 style={{ textAlign: "center", marginTop: "100px", color: "white" }}>
+          Loading...
+        </h1>
+      }
+    >
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </Suspense>
   );
 }
 
